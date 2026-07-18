b0bc5de40b19429771578a2b24474ff516f2ae33
Septino93 <vsh5b2yfgj@privaterelay.appleid.com>
2026-07-18 11:51:44 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.CF_CONFIG = Object.freeze({
-  supabaseUrl: 'https://jjfnuqwjucqirmrgfjne.supabase.co',
-  supabasePublishableKey: 'sb_publishable_MyVpSs485ZeYs31LtNo8RQ_5pjRTB79'
-});
--- js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  const cfg=window.CF_CONFIG||{};
-  if(!window.supabase) throw new Error('Library Supabase belum dimuat.');
-  if(!cfg.supabaseUrl||!cfg.supabasePublishableKey) throw new Error('Konfigurasi Supabase belum lengkap.');
-  window.cfSupabase=window.supabase.createClient(cfg.supabaseUrl,cfg.supabasePublishableKey,{
-    auth:{persistSession:true,autoRefreshToken:true,detectSessionInUrl:true}
-  });
-})();
